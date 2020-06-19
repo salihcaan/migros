3c82c242af21e6224a352dfcb52f8cbbe1882beb
alicank07 <alican.kurt.p@gmail.com>
2020-06-19 17:14:08 +0300

    Added Some Functions
    Added Some Functions

--- src/test/java/pages/common/MainPage.java
+++ src/test/java/pages/common/MainPage.java
@@ -20,6 +20,30 @@ public class MainPage extends AbstractPage
 
     @FindBy(css = "a[data-monitor-ga-action='Kırmızı Et']")
     public WebElement meatCategory;
+    
+    // Enter the "Bebek, Oyuncak" menü
+    @FindBy(linkText = "Bebek, Oyuncak")
+    public WebElement bebekOyuncakMenu;
+
+    // Select the Category "Bebek Bezi"
+    @FindBy(css = "a[data-monitor-ga-action='Bebek Bezi']")
+    public WebElement bebekBeziCategory;
+
+   // Select the Brand -Prima-
+    @FindBy(css = "a[href='prima-b-390/bebek-bezi-c-ab']")
+    public WebElement selectBebekBeziBrand;
+
+    // Select the 4-Beden
+    @FindBy(css = "a[data-monitor-ga-label='4 Beden']")
+    public WebElement check4BedenClick;
+
+    // Open the Sorting List
+    @FindBy(css = ".priority-sort-bar .others-button")
+    public WebElement openSortingList;
+
+    // Sorting Max Price
+    @FindBy(xpath = "//*[@id='product-list-sort']/nav/ul[1]/li[5]/a")
+    public WebElement sortingMaxPrice;
 
     @FindBy(css = ".shoping-cart-icon-block .fa-shopping-cart")
     public WebElement shoppingBasketButton;
